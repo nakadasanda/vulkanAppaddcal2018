22467c862a849655bb7c1d440850f2cfa5b1c195
nakadasanda <nakadasanda@outlook.com>
2018-12-24 00:35:10 +0900

    refactoring

--- vulkanAppAdcal2018/Renderer.cpp
+++ vulkanAppAdcal2018/Renderer.cpp
@@ -1,8 +1,11 @@
 #include "Renderer.h"
-
+#include <iostream>
 #include  <cstdlib>
 #include <assert.h>
 #include <vector>
+#include <sstream>
+#include <Windows.h>
+
 
 Renderer::Renderer()
 {
@@ -19,11 +22,55 @@ Renderer::~Renderer()
 	system("pause");
 }
 
+void printDeviceStatus(VkPhysicalDevice &gpu) {
+	VkPhysicalDeviceProperties gpu_properties;
+	VkPhysicalDeviceFeatures features_properties;
+	VkPhysicalDeviceMemoryProperties memory_properties;
+	uint32_t queue_families_count = 0;
+	vkGetPhysicalDeviceProperties( gpu,&gpu_properties);
+	uint32_t apiVer = gpu_properties.apiVersion;
+	std::cout << "Name:\t" << gpu_properties.deviceName << std::endl;
+	std::cout << "API Version:\t" << VK_VERSION_MAJOR(apiVer) << "." <<VK_VERSION_MINOR(apiVer) << "." <<VK_VERSION_PATCH(apiVer) << std::endl;
+	std::cout << "Driver Version:\t" << gpu_properties.driverVersion << std::endl;
+	std::cout << "Vendor ID:\t" << gpu_properties.vendorID << std::endl;
+	std::cout << "Device ID:\t" << gpu_properties.deviceID << std::endl;
+	std::cout << "Device Type:\t" << gpu_properties.deviceType << std::endl;
+
+	vkGetPhysicalDeviceFeatures(gpu, &features_properties);
+	vkGetPhysicalDeviceMemoryProperties(gpu, &memory_properties);
+	
+	
+	vkGetPhysicalDeviceQueueFamilyProperties(gpu, &queue_families_count, nullptr);
+	std::vector<VkQueueFamilyProperties> family_property(queue_families_count);
+	std::cout << "QueufamilyCount:\t" << queue_families_count << std::endl;
+	vkGetPhysicalDeviceQueueFamilyProperties(gpu, &queue_families_count, family_property.data());
+
+	for (uint32_t i = 0; i <queue_families_count; ++i){
+		std::cout << std::endl;
+		std::cout << "Queue Family #" << i << std::endl;
+		std::cout << "VK_QUEUE_GRAPHICS_BIT:\t" << ((family_property[i].queueFlags &VK_QUEUE_GRAPHICS_BIT) != 0) << std::endl;
+		std::cout << "&VK_QUEUE_COMPUTE_BIT:\t" << ((family_property[i].queueFlags &VK_QUEUE_COMPUTE_BIT) != 0) << std::endl;
+		std::cout << "VK_QUEUE_TRANSFER_BIT:\t" << ((family_property[i].queueFlags &VK_QUEUE_TRANSFER_BIT) != 0) << std::endl;
+		std::cout << "VK_QUEUE_SPARSE_BINDING_BIT:\t" << ((family_property[i].queueFlags &VK_QUEUE_SPARSE_BINDING_BIT) != 0) << std::endl;
+		std::cout << "QUEUE COUNT:\t" << family_property[i].queueCount << std::endl;
+		std::cout << "timestampValidBits:\t" << family_property[i].timestampValidBits << std::endl;
+		uint32_t width = family_property[i].minImageTransferGranularity.width;
+		uint32_t height = family_property[i].minImageTransferGranularity.height;
+		uint32_t depth = family_property[i].minImageTransferGranularity.depth;
+		std::cout << "minImageTransferGranularity:\t" << width << ", "<< height<<", " << depth << std::endl;
+
+	}
+	
+
+	std::cout << std::endl;
+}
+
 void Renderer::_InitInstance()
 {
 	VkApplicationInfo application_info{};
 	application_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	application_info.apiVersion = VK_API_VERSION_1_0;
+	application_info.apiVersion = VK_MAKE_VERSION(1, 0, 3);
+	application_info.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
 	application_info.pApplicationName = "VulkanAppAdcal2018";
 
 	VkInstanceCreateInfo instance_create_info{};
@@ -40,41 +87,24 @@ void Renderer::_DeInitInstance()
 {
 	vkDestroyInstance(_instance, nullptr);
 	_instance = nullptr;
+
 }
+
 
 void Renderer::_InitDevise()
 {
-	{
-		uint32_t gpu_count = 0;
-		vkEnumeratePhysicalDevices(_instance, &gpu_count, nullptr);
-		std::vector<VkPhysicalDevice> gpu_list(gpu_count);
-		vkEnumeratePhysicalDevices(_instance, &gpu_count, gpu_list.data());
-		_gpu = gpu_list[0];
-		vkGetPhysicalDeviceProperties(_gpu, &_gpu_property);
-	}
-	{
-		uint32_t family_count = 0;
-		vkGetPhysicalDeviceQueueFamilyProperties(_gpu,&family_count,nullptr);
-		std::vector<VkQueueFamilyProperties> family_property_list(family_count);
-		vkGetPhysicalDeviceQueueFamilyProperties(_gpu, &family_count, family_property_list.data());
+	uint32_t gpu_count = 0;
+	vkEnumeratePhysicalDevices(_instance, &gpu_count, nullptr);
+	std::vector<VkPhysicalDevice> gpu_list(gpu_count);
+	std::cout << "gpu_count = "<< gpu_count <<std::endl;
+	vkEnumeratePhysicalDevices(_instance, &gpu_count, gpu_list.data());
+	_gpu = gpu_list[0]; // TODO Choose best Device;
 
-		bool found = false;
-		for (uint32_t i = 0; i < family_count; ++i) {
-			if (family_property_list[i].queueFlags  & VK_QUEUE_GRAPHICS_BIT) {
-				found = true;
-				_graphics_family_index = 1;
-			}
-		}
-		if (!found) {
-			assert(0 && "Vulkan err: Queue Family supporting graphics not faound");
-		std:exit(-1);
-		}
-	}
-	float queue_priorities[]{1.0f};
+	float queue_priorities[]{ 1.0f };
 	VkDeviceQueueCreateInfo device_queue_create_info{};
 	device_queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-	device_queue_create_info.queueFamilyIndex = _graphics_family_index;
-	device_queue_create_info.queueCount = 1;
+	device_queue_create_info.queueFamilyIndex = 0;// TODO Choose correct family index
+	device_queue_create_info.queueCount = 4; // TODO  Check if this amount is valid
 	device_queue_create_info.pQueuePriorities = queue_priorities;
 
 	VkDeviceCreateInfo device_create_info{};
@@ -86,6 +116,7 @@ void Renderer::_InitDevise()
 		assert(0 && "Vulkan Err :Device Create failed");
 		std::exit(-1);
 	}
+	printDeviceStatus(_gpu);
 }
 
 void Renderer::_DeInitDevice()
--- vulkanAppAdcal2018/Renderer.h
+++ vulkanAppAdcal2018/Renderer.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vulkan\vulkan.h>
+#include <vector>
+
 class Renderer
 {
 public:
@@ -12,12 +14,13 @@ private:
 
 	void _InitDevise();
 	void _DeInitDevice();
-	
+
 	VkInstance	_instance = nullptr;
 	VkPhysicalDevice _gpu = nullptr;
 	VkDevice	_devise = nullptr;
 	VkPhysicalDeviceProperties _gpu_property = {};
 	
 	uint32_t _graphics_family_index=0;
+
 };
 
