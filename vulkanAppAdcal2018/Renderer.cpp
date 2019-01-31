e7b1ed23ae7c5db07ec1e999693b56261da5cf8a
nakadasanda <nakadasanda@outlook.com>
2019-01-31 16:46:00 +0900

    ~refactoring~

--- vulkanAppAdcal2018/Renderer.cpp
+++ vulkanAppAdcal2018/Renderer.cpp
@@ -70,6 +70,8 @@ const VkPhysicalDeviceProperties & Renderer::GetVulkanPhysicalDeviceProperties()
 }
 void Renderer::_SetupLayersAndExtensions()
 {
+	_instance_layers.push_back("VK_LAYER_LUNARG_standard_validation");
+
 	_instance_extensions.push_back(VK_KHR_SURFACE_EXTENSION_NAME);
 	_instance_extensions.push_back(VK_KHR_WIN32_SURFACE_EXTENSION_NAME);
 }
@@ -78,9 +80,13 @@ void printDeviceStatus(VkPhysicalDevice &gpu) {
 	VkPhysicalDeviceProperties gpu_properties;
 	VkPhysicalDeviceFeatures features_properties;
 	VkPhysicalDeviceMemoryProperties memory_properties;
+
 	uint32_t queue_families_count = 0;
 	vkGetPhysicalDeviceProperties( gpu,&gpu_properties);
 	uint32_t apiVer = gpu_properties.apiVersion;
+
+	std::cout << "gpu_Properties" << std::endl;
+	std::cout << std::endl;
 	std::cout << "Name:\t" << gpu_properties.deviceName << std::endl;
 	std::cout << "API Version:\t" << VK_VERSION_MAJOR(apiVer) << "." <<VK_VERSION_MINOR(apiVer) << "." <<VK_VERSION_PATCH(apiVer) << std::endl;
 	std::cout << "Driver Version:\t" << gpu_properties.driverVersion << std::endl;
@@ -88,15 +94,21 @@ void printDeviceStatus(VkPhysicalDevice &gpu) {
 	std::cout << "Device ID:\t" << gpu_properties.deviceID << std::endl;
 	std::cout << "Device Type:\t" << gpu_properties.deviceType << std::endl;
 
+	std::cout << std::endl;
+
 	vkGetPhysicalDeviceFeatures(gpu, &features_properties);
 	vkGetPhysicalDeviceMemoryProperties(gpu, &memory_properties);
 	
+
 	
 	vkGetPhysicalDeviceQueueFamilyProperties(gpu, &queue_families_count, nullptr);
 	std::vector<VkQueueFamilyProperties> family_property(queue_families_count);
 	std::cout << "QueufamilyCount:\t" << queue_families_count << std::endl;
-	vkGetPhysicalDeviceQueueFamilyProperties(gpu, &queue_families_count, family_property.data());
+	std::cout << std::endl;
 
+	vkGetPhysicalDeviceQueueFamilyProperties(gpu, &queue_families_count, family_property.data());
+	std::cout << "QueueFamilyProperties" << std::endl;
+	std::cout << std::endl;
 	for (uint32_t i = 0; i <queue_families_count; ++i){
 		std::cout << std::endl;
 		std::cout << "Queue Family #" << i << std::endl;
@@ -112,7 +124,42 @@ void printDeviceStatus(VkPhysicalDevice &gpu) {
 		std::cout << "minImageTransferGranularity:\t" << width << ", "<< height<<", " << depth << std::endl;
 
 	}
+
+	uint32_t extensions_count = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensions_count, nullptr);
+	std::vector<VkExtensionProperties> extensions(extensions_count);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensions_count, extensions.data());
+
+	std::cout << std::endl;
+	std::cout << "Amount of Extensions " << extensions_count << std::endl;
+	std::cout << std::endl;
+	std::cout << "ExtensionProperties" << std::endl;
+	std::cout << std::endl;
+
+	for (int i = 0; i < extensions_count; i++) {
+		std::cout << std::endl;
+		std::cout << "Name:        " << extensions[i].extensionName << std::endl;
+		std::cout << "specVersion: " << extensions[i].specVersion << std::endl;
+
+	}
+	std::cout << std::endl;
 	
+	uint32_t layer_count = 0;
+	vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+
+	std::vector<VkLayerProperties> layers(layer_count);
+
+	vkEnumerateInstanceLayerProperties(&layer_count, layers.data());
+
+	std::cout << "Amount of Layers " << layer_count << std::endl;
+	std::cout << std::endl;
+	for (int i = 0; i < layer_count; i++) {
+		std::cout << std::endl;
+		std::cout << "layerName:            " << layers[i].layerName << std::endl;
+		std::cout << "specVersion:          " << layers[i].specVersion << std::endl;
+		std::cout << "implementationVersion:" << layers[i].implementationVersion << std::endl;
+		std::cout << "description:          " << layers[i].description << std::endl;
+	}
 
 	std::cout << std::endl;
 }
@@ -127,53 +174,15 @@ void Renderer::_InitInstance()
 	application_info.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
 	application_info.pApplicationName = "VulkanAppAdcal2018";
 
-	uint32_t layer_count = 0;
-	vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
-	
-	std::vector<VkLayerProperties> layers(layer_count);
-
-	vkEnumerateInstanceLayerProperties(&layer_count, layers.data());
-
-	std::cout << "Amount of Layers " << layer_count << std::endl;
-	for (int i = 0; i < layer_count; i++) {
-		std::cout << std::endl;
-		std::cout << "layerName:            " << layers[i].layerName << std::endl;
-		std::cout << "specVersion:          " << layers[i].specVersion << std::endl;
-		std::cout << "implementationVersion:" << layers[i].implementationVersion << std::endl;
-		std::cout << "description:          " << layers[i].description << std::endl;
-	}
-
-	uint32_t extensions_count = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensions_count, nullptr);
-	std::vector<VkExtensionProperties> extensions(extensions_count);
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensions_count, extensions.data());
-
-	std::cout << std::endl;
-	std::cout << "Amount of Extensions " << extensions_count << std::endl;
-	for (int i = 0; i < extensions_count; i++) {
-		std::cout << std::endl;
-		std::cout << "Name:        " << extensions[i].extensionName<< std::endl;
-		std::cout << "specVersion: " << extensions[i].specVersion << std::endl;
-		
-	}
-
-	const std::vector<const char*>validationLayers = {
-		"VK_LAYER_LUNARG_standard_validation"
-	};
-
-
 	VkInstanceCreateInfo instance_create_info{};
 	instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	instance_create_info.pNext = nullptr;
 	instance_create_info.flags = 0;
 	instance_create_info.pApplicationInfo = &application_info;
-	instance_create_info.enabledExtensionCount = validationLayers.size();
-	instance_create_info.ppEnabledLayerNames = validationLayers.data();
+	instance_create_info.enabledExtensionCount = _instance_layers.size();
+	instance_create_info.ppEnabledLayerNames = _instance_layers.data();
 	instance_create_info.enabledExtensionCount = _instance_extensions.size();
 	instance_create_info.ppEnabledExtensionNames = _instance_extensions.data();
-	//instance_create_info.enabledExtensionCount = usedExtensions.size();
-	//instance_create_info.ppEnabledExtensionNames = usedExtensions.data();
-
 
 	auto err = vkCreateInstance(&instance_create_info, nullptr, &_instance);
 	if (VK_SUCCESS != err) {
@@ -182,12 +191,7 @@ void Renderer::_InitInstance()
 	}
 }
 
-void Renderer::_DeInitInstance()
-{
-	vkDestroyInstance(_instance, nullptr);
-	_instance = nullptr;
 
-}
 
 
 void Renderer::_InitDevise()
@@ -196,14 +200,15 @@ void Renderer::_InitDevise()
 	vkEnumeratePhysicalDevices(_instance, &gpu_count, nullptr);
 	std::vector<VkPhysicalDevice> gpu_list(gpu_count);
 	std::cout << "gpu_count = "<< gpu_count <<std::endl;
+	std::cout << std::endl;
 	vkEnumeratePhysicalDevices(_instance, &gpu_count, gpu_list.data());
-	_gpu = gpu_list[0]; // TODO Choose best Device;
+	_gpu = gpu_list[0]; 
 
 	float queue_priorities[]{ 1.0f};
 	VkDeviceQueueCreateInfo device_queue_create_info{};
 	device_queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-	device_queue_create_info.queueFamilyIndex = 0;// TODO Choose correct family index
-	device_queue_create_info.queueCount = 1; // TODO  Check if this amount is valid
+	device_queue_create_info.queueFamilyIndex = 0;
+	device_queue_create_info.queueCount = 1; 
 	device_queue_create_info.pQueuePriorities = queue_priorities;
 
 	VkDeviceCreateInfo device_create_info{};
@@ -222,6 +227,13 @@ void Renderer::_InitDevise()
 	vkDeviceWaitIdle(_device);
 	
 	vkGetDeviceQueue(_device, 0, 0, &_queue);
+
+}
+
+void Renderer::_DeInitInstance()
+{
+	vkDestroyInstance(_instance, nullptr);
+	_instance = nullptr;
 
 }
 
--- vulkanAppAdcal2018/Window.cpp
+++ vulkanAppAdcal2018/Window.cpp
@@ -1,6 +1,6 @@
 #include "Window.h"
 #include <assert.h>
-
+#include <iostream>
 
 
 Window::Window(Renderer * renderer, uint32_t size_x, uint32_t size_y, std::string name)
@@ -17,7 +17,10 @@ Window::Window(Renderer * renderer, uint32_t size_x, uint32_t size_y, std::strin
 
 Window::~Window()
 {
+	_DeInitSurface();
+
 	_DeInitOSWindow();
+
 }
 
 void Window::Close()
@@ -39,7 +42,19 @@ void Window::_InitSurface()
 	VkBool32 WSI_supported = false;
 	vkGetPhysicalDeviceSurfaceSupportKHR(gpu, _renderer->GetVulkanQueueFamilyIndex(), _surface, &WSI_supported);
 	
+	std::cout << std::endl;
+	std::cout << "DeviceSurfaceCapabilities" << std::endl;
 	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(gpu, _surface, &_surface_capabilities);
+	std::cout << "minImageCount " << _surface_capabilities.minImageCount << std::endl;
+	std::cout << "maxImageCount " << _surface_capabilities.maxImageCount << std::endl;
+	std::cout << "currentExtent: (width, height) " << "( "<< _surface_capabilities.currentExtent.width << ", " << _surface_capabilities.currentExtent.height << " )" << std::endl;
+	std::cout << "minImageExtent: (width, height) " << "( " << _surface_capabilities.minImageExtent.width << ", " << _surface_capabilities.minImageExtent.height << " )" << std::endl;
+	std::cout << "maxImageArrayLayers " << _surface_capabilities.maxImageArrayLayers << std::endl;
+	std::cout << "supportedCompositeAlpha" << _surface_capabilities.supportedCompositeAlpha << std::endl;
+	std::cout << "supportedTransforms " << _surface_capabilities.supportedTransforms << std::endl;
+	std::cout << "currentTransform " << _surface_capabilities.currentTransform << std::endl;
+	std::cout << " supportedCompositeAlpha " << _surface_capabilities.supportedCompositeAlpha << std::endl;
+	std::cout << "supportedUsageFlags" << _surface_capabilities.supportedUsageFlags << std::endl;
 	{
 		uint32_t format_count = 0;
 		vkGetPhysicalDeviceSurfaceFormatsKHR(gpu, _surface, &format_count, nullptr);
